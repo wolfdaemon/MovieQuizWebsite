fd3b2588b33c564ac280ad93eff3c50e185d327c
Michael "Cal" Krug <cal.krug@proton.me>
2022-07-11 19:59:24 -0500

    Attempted (broken) link with React and Spring Boot

---
+++ movie-quiz-website/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import { unregister } from "./service-worker";
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
+service-worker.unregister();
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
